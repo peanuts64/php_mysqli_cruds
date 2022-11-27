ffff6f54be5e6ed184b26ac01eeb0c461669a6e7
asa <asaralvarado@gmail.com>
2022-11-26 17:58:26 -0800

    changed mysql to mysqli

--- mysqli/includes/actions.php
+++ mysqli/includes/actions.php
@@ -3,36 +3,37 @@
 class Dbactions {
   
   /* Variables for DB Connect */
-  private $db_host = "localhost";    // Hostname
-  private $db_user = "root";         // Username
-  private $db_pass = "";             // Password
-  private $db_name = "test";         // Database
+  private $db_host = "127.0.0.1";    // Hostname
+  private $db_user = "admin";         // Username
+  private $db_pass = "taco";             // Password
+  private $db_name = "asatech";         // Database
   
   /* Extra variables that are required by other functions */
   private $con = false;              // Checks connection is active
   private $result = array();         // Returns the query result
   private $myQuery = "";             // Process SQL Query
   private $numResults = "";          // Returns the number of rows
-  
+  private $conn;
   /* Function to make connection to database */
   public function __construct() {
     if (!$this->con) {
       // mysql_connect() with variables defined at the start of Database class
-      $myconn = mysql_connect($this->db_host,$this->db_user,$this->db_pass);
+      $myconn = mysqli_connect($this->db_host,$this->db_user,$this->db_pass);
       if ($myconn) {
         // Credentials have been pass through mysql_connect() now select the database
-        $seldb = mysql_select_db($this->db_name,$myconn);
+        $seldb = $myconn->select_db($this->db_name);
         if ($seldb) {
           $this->con = true;
+	  $this->conn = $myconn;
           // Return TRUE, when the Connection has been made 
           return true;                              
         } else {
-          array_push($this->result,mysql_error()); 
+          array_push($this->result,mysqli_error()); 
           // Return FALSE, when there's a problem in selecting database
           return false;
         }  
       } else {
-        array_push($this->result,mysql_error());
+        array_push($this->result,mysqli_error());
         // Return FALSE, Problem in connecting
         return false;
       }  
@@ -45,7 +46,7 @@ class Dbactions {
   /* Function to disconnect the database */
   public function disconnect() {
     if ($this->con) {
-      if (mysql_close()) {
+      if (mysqli_close()) {
         $this->con = false;
         return true;
       } else {
@@ -56,21 +57,21 @@ class Dbactions {
   
   /* Function to process an sql query */
   public function sql($sql) {
-    $query = mysql_query($sql);
+    $query = mysqli_query($sql);
     $this->myQuery = $sql;
 
     // If the query returns >= 1 assign the number of rows to numResults                              
     if ($query) {
-      $this->numResults = mysql_num_rows($query); 
+      $this->numResults = mysqli_num_rows($query); 
 
       // Loop through the query results by the number of rows returned
       for ($i = 0; $i < $this->numResults; $i++) {
-        $r = mysql_fetch_array($query);
+        $r = mysqli_fetch_array($query);
         $key = array_keys($r);
         for ($x = 0; $x < count($key); $x++) {                                      
           // Sanitizes keys so only alphavalues are allowed
           if (!is_int($key[$x])) {
-            if (mysql_num_rows($query) >= 1) {
+            if (mysqli_num_rows($query) >= 1) {
               $this->result[$i][$key[$x]] = $r[$key[$x]];
             } else {
               $this->result = null;
@@ -80,16 +81,16 @@ class Dbactions {
       }
       return true;
     } else {
-      array_push($this->result,mysql_error());
+      array_push($this->result,mysqli_error());
       return false;
     }
   }
   
   /* Private function to check if table exists for use with queries */
   private function tableExists($table) {
-    $tablesInDb = mysql_query('SHOW TABLES FROM '.$this->db_name.' LIKE "'.$table.'"');
+    $tablesInDb = $this->conn->query('SHOW TABLES FROM '.$this->db_name.' LIKE "'.$table.'"');
     if ($tablesInDb) {
-      if (mysql_num_rows($tablesInDb)==1) {
+      if (mysqli_num_rows($tablesInDb)==1) {
         return true; // The table exists
       } else {
         array_push($this->result,$table." does not exist in this database");
@@ -105,12 +106,12 @@ class Dbactions {
       $sql='INSERT INTO `'.$table.'` (`'.implode('`, `',array_keys($params)).'`) VALUES (\'' . implode('\', \'', $params) . '\')';
       $this->myQuery = $sql; 
       // The data has been inserted      
-      if ($ins = mysql_query($sql)) {
-        array_push($this->result,mysql_insert_id());
+      if ($ins = mysqli_query($sql)) {
+        array_push($this->result,mysqli_insert_id());
         return true;
       } else {
         // The data has not been inserted
-        array_push($this->result,mysql_error());
+        array_push($this->result,mysqli_error());
         return false;
       }
     } else {
@@ -137,16 +138,16 @@ class Dbactions {
     
     // If the table exists
     if ($this->tableExists($table)) {
-      $query = mysql_query($q);
+      $query = mysqli_query($q);
       // If the query returns >= 1 assign the number of rows to numResults
       if ($query) {
-        $this->numResults = mysql_num_rows($query);
-        while( $results[] = mysql_fetch_object($query));
+        $this->numResults = mysqli_num_rows($query);
+        while( $results[] = mysqli_fetch_object($query));
         array_pop ( $results );
         $this->result = $results;
         return true; // Query was successful
       } else {
-        array_push($this->result,mysql_error());
+        array_push($this->result,mysqli_error());
         return false; // No rows where returned
       }
     } else{
@@ -155,7 +156,7 @@ class Dbactions {
   }
   
   /* Function to update row in database */
-  public function update($table,$params=array(),$where){
+  public function updates($table,$where,$params=array()){
     // If the table exists
     if ($this->tableExists($table)) {
       $args=array();
@@ -166,11 +167,11 @@ class Dbactions {
       // Create the query
       $sql='UPDATE '.$table.' SET '.implode(',',$args).' WHERE '.$where;
       $this->myQuery = $sql; 
-      if ($query = mysql_query($sql)) {
-        array_push($this->result,mysql_affected_rows());
+      if ($query = mysqli_query($sql)) {
+        array_push($this->result,mysqli_affected_rows());
         return true; // Update has been successful
       } else {
-        array_push($this->result,mysql_error());
+        array_push($this->result,mysqli_error());
         return false; // Update has not been successful
       }
     } else {
@@ -190,13 +191,13 @@ class Dbactions {
         $delete = 'DELETE FROM '.$table.' WHERE '.$where;
       }
             
-      if ($del = mysql_query($delete)) {
-        array_push($this->result,mysql_affected_rows());
+      if ($del = mysqli_query($delete)) {
+        array_push($this->result,mysqli_affected_rows());
         $this->myQuery = $delete; 
         // The query exectued correctly
         return true;
       } else {
-        array_push($this->result,mysql_error());
+        array_push($this->result,mysqli_error());
         // The query did not execute correctly
         return false;
       }
@@ -207,7 +208,7 @@ class Dbactions {
   
   
   /* Function to import csv data into mysql databse */
-  public function csv_import($file_name, $table_name, $columns=array(), $file_heading){
+  public function csv_imports($file_name, $table_name, $file_heading, $columns=array() ){
     $field_separate_char = ",";   // separation character   
     $field_enclose_char  = "\"";  // enclose character
     $field_escape_char   = "\\";  // escape character
@@ -215,16 +216,16 @@ class Dbactions {
     if (!empty($columns)) {
       // If table exists
       if ($this->tableExists($table_name)) {
-        $sql = "LOAD DATA INFILE '".@mysql_escape_string($file_name).
+        $sql = "LOAD DATA INFILE '".@mysqli_escape_string($file_name).
          "' INTO TABLE `".$table_name.
-         "` FIELDS TERMINATED BY '".@mysql_escape_string($field_separate_char).
-         "' OPTIONALLY ENCLOSED BY '".@mysql_escape_string($field_enclose_char).
-         "' ESCAPED BY '".@mysql_escape_string($field_escape_char).
+         "` FIELDS TERMINATED BY '".@mysqli_escape_string($field_separate_char).
+         "' OPTIONALLY ENCLOSED BY '".@mysqli_escape_string($field_enclose_char).
+         "' ESCAPED BY '".@mysqli_escape_string($field_escape_char).
          "' ".
          ($file_heading ? " IGNORE 1 LINES " : "")
          ."(`".implode("`,`", $columns)."`)";
         
-        $res = @mysql_query($sql);
+        $res = @mysqli_query($sql);
         return $res;
       } else {
         return false;
@@ -263,18 +264,18 @@ class Dbactions {
         echo "\r\n";
       }
   
-      $query = @mysql_query($q);
+      $query = @mysqli_query($q);
       if ($query) {
         //Following headers instruct the browser to treat the data as a csv file called export.csv
         header('Content-Type: text/csv');
         header('Content-Disposition: attachment; filename=data.csv');
 
-        $row = mysql_fetch_assoc($query);
+        $row = mysqli_fetch_assoc($query);
         if ($row) {
           gen_csv(array_keys($row));
           while ($row) {
             gen_csv($row);
-            $row = mysql_fetch_assoc($query);
+            $row = mysqli_fetch_assoc($query);
           }
           exit;
         }
