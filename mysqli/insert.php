e07fbd9035af30a29c98cc361f356d4bdbe8a356
asa <asaralvarado@gmail.com>
2022-11-29 15:56:23 -0800

    added a form to inser.php

--- mysqli/insert.php
+++ mysqli/insert.php
@@ -1,10 +1,28 @@
 <?php
 	
 	include('includes/actions.php');
+	include('includes/dom.inc.php');
+	include('includes/template.inc.php');
+	include('includes/dbh.inc.php');
 	global $sadb;
+	$form = new Dom();
+	$inputs = array( "User_Name" => "text", "Password_hash" => "password", "email" => "text");
+############################################################
+#$this->form_new_list($array($tag = $type)) returns $this->form = $form;
+# Data flag           $this->post_data_inputs = $insert;
+# Error flag array    $this->input_flag = $data_Err;
+#############################################################
+$form->form_new_list($inputs);
+echo $form->form;
+#print_r($form->post_data_inputs);
+#echo $form->input_flag;
+#  public function updates($table,$where,$params=array()){
+	$form->post_data_inputs['Password_hash'] = (isset($form->post_data_inputs) ? password_hash($form->post_data_inputs['Password_hash'], PASSWORD_DEFAULT) : '');
+	$status = ($form->input_flag === 'TRUE' ?	
+	$sadb->insert('Users',$form->post_data_inputs) // Table name, column names and values, WHERE conditions
+	:
+	'someting went wrog ');
 	
-#  public function insert($table,$params=array()) {
-	$sadb->insert('Users',array('User_Name'=>'Name 5', 'Password_hash' => 'hash','email'=>'name5@email.com')); // Table name, column names and respective values
 	$res = $sadb->getResult();
 	
 	echo "<pre>";
